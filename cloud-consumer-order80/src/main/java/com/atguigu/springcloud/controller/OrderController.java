651f31169d80fecae972c29f6ce618ee1face281
Sunhaibo012 <2245154826@qq.com>
2020-05-13 23:43:40 +0800

    工程重构，删除entities

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,42 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther hiber
+ * @create 2020/5/13
+ */
+@RestController
+@Slf4j
+public class OrderController {
+    //调8001  RestTemplate提供多种方法便捷访问远程http服务的方法，是spring的一个工具集，调用服务器接口
+    /*
+    * RestTemplate访问restful接口非常简单
+    * (url,requestMap,ResponseBean.class)这三个参数分别代表rest请求地址、请求参数、HTTP响应转换被转换成的对象类型
+    * */
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        log.info("*****80接口插入");
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        log.info("******80接口查询");
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+}
